792f15adc73166fcb1963377ea8354232d7d74bd
korhankosece <korhankosece@gmail.com>
2020-12-03 14:38:32 +0300

    Person List section added

---
+++ ProjectIMDB/Controllers/PersonController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectIMDB.Models.ORM.Context;
+using ProjectIMDB.Models.VM;
+
+namespace ProjectIMDB.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly IMDBContext _context;
+
+        public PersonController(IMDBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<PersonListVM> personLists = _context.People.Where(q => q.IsDeleted == false).Select(q => new PersonListVM()
+            {
+                id = q.ID,
+                name = q.Name,
+                surname = q.SurName,
+                birthdate = q.BirthDate,
+                nationality = q.Nationality,
+                adddate = q.AddDate,
+                updatedate = q.UpdateDate,
+                isdeleted = q.IsDeleted,
+
+            }).ToList();
+
+            return View(personLists);
+
+        }
+    }
+}
