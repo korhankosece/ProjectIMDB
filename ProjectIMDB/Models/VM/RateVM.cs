28f1623b44f8f7522b3ad654e37b6b7846228346
zelaltuncel <zelaltuncel@hotmail.com>
2020-12-05 16:12:46 +0300

    Rate Crud Operations

---
+++ ProjectIMDB/Models/VM/RateVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectIMDB.Models.VM
+{
+    public class RateVM
+    {
+        public int id { get; set; }
+        public double point { get; set; }
+        public string moviename { get; set; }
+        public string username { get; set; }
+        public DateTime adddate { get; set; }
+        public DateTime updatedate { get; set; }
+    }
+}
