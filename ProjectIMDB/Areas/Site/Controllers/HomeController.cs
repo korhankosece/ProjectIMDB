4f41e35aa1a0469946fe91957d08c1b9088eb4a3
zelaltuncel <zelaltuncel@hotmail.com>
2020-12-24 17:56:10 +0300

    Listing of last 10 movies on Home page

--- ProjectIMDB/Areas/Site/Controllers/HomeController.cs
+++ ProjectIMDB/Areas/Site/Controllers/HomeController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectIMDB.Models.ORM.Context;
+using ProjectIMDB.Models.VM;
 
 namespace ProjectIMDB.Areas.Site.Controllers
 {
     [Area("Site")]
     public class HomeController : Controller
     {
+        private readonly IMDBContext _context;
+
+        public HomeController(IMDBContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            SiteHomeVM model = new SiteHomeVM();
+            model.BannerMovies = _context.Movies.Include(x => x.MovieGenres).ThenInclude(MovieGenres => MovieGenres.Genre).Include(x => x.MoviePeople).ThenInclude(MoviePerson => MoviePerson.Person).Where(q => q.IsDeleted==false).OrderByDescending(q => q.ID).Take(10).ToList();
+
+           
+
+            
+            
+            return View(model);
+
+            
         }
     }
 }
---
+++ ProjectIMDB/Models/VM/SiteHomeVM.cs
@@ -0,0 +1,13 @@
+﻿using ProjectIMDB.Models.ORM.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectIMDB.Models.VM
+{
+    public class SiteHomeVM
+    {
+        public List<Movie> BannerMovies { get; set; }
+    }
+}
