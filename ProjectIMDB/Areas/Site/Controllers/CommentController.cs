6c21fe4daecb97c789f4e7a1bfaa48218e2e52d9
zelaltuncel <zelaltuncel@hotmail.com>
2021-02-06 17:53:59 +0300

    Comment route and delete operations  are revised

--- ProjectIMDB/Areas/Site/Controllers/CommentController.cs
+++ ProjectIMDB/Areas/Site/Controllers/CommentController.cs
@@ -9,6 +9,7 @@ using ProjectIMDB.Models.Attributes;
 using ProjectIMDB.Models.ORM.Context;
 using ProjectIMDB.Models.ORM.Entities;
 using ProjectIMDB.Models.VM;
+using ProjectIMDB.Models.Helpers;
 
 namespace ProjectIMDB.Areas.Site.Controllers
 {
@@ -30,20 +31,20 @@ namespace ProjectIMDB.Areas.Site.Controllers
         public IActionResult Add(int id)
         {
             CommentVM model = new CommentVM();
-            
-            
+
+
 
             int a = Convert.ToInt32(TempData["ID"]);
             User user = _context.Users.Where(q => q.IsDeleted == false).FirstOrDefault(q => q.ID == a);
-            
+
             model.movieid = id;
-            model.username =user.UserName;
+            model.username = user.UserName;
             return View(model);
         }
 
 
 
-      
+
         [HttpPost]
 
         public IActionResult Add(CommentVM model)
@@ -54,13 +55,18 @@ namespace ProjectIMDB.Areas.Site.Controllers
             comment.MovieID = model.movieid;
             comment.Header = model.header;
 
+            int id = model.movieid;
+            Movie movie = _context.Movies.Include(q => q.Comments.Where(q => q.IsDeleted == false)).FirstOrDefault(q => q.ID == id);
+            var name = movie.Name;
 
 
 
             _context.Comments.Add(comment);
             _context.SaveChanges();
 
-            return RedirectToAction("Index", "Home");
+            string url = "/movie/" + id + "/" + UrlHelper.FriendlyUrl(name);
+            return Redirect(url);
+
         }
 
 
@@ -75,47 +81,58 @@ namespace ProjectIMDB.Areas.Site.Controllers
             model.content = comment.Content;
             model.movieid = comment.MovieID;
             model.id = comment.ID;
-           
+
+
             return View(model);
         }
 
 
 
- 
+
 
         [HttpPost]
 
         public IActionResult Edit(CommentVM model)
         {
-            Comment comment = _context.Comments.FirstOrDefault(q =>q.ID == model.id);
+            Comment comment = _context.Comments.FirstOrDefault(q => q.ID == model.id);
             comment.Header = model.header;
             comment.Content = model.content;
             comment.MovieID = model.movieid;
             //comment.UserID = Convert.ToInt32(ViewBag.ID);
 
-
+            var id = comment.MovieID;
+            Movie movie = _context.Movies.Include(q => q.Comments.Where(q => q.IsDeleted == false)).FirstOrDefault(q => q.ID == id);
+            var name = movie.Name;
 
             _context.SaveChanges();
+            string url = "/movie/" + id + "/" + UrlHelper.FriendlyUrl(name);
+            return Redirect(url);
 
-            return RedirectToAction("Index", "Home");
 
 
-            
-          
+
         }
 
 
-      
+
 
         [HttpPost]
 
-        public IActionResult Delete(int id)
+        public IActionResult Delete(int idb)
         {
-            Comment comment = _context.Comments.FirstOrDefault(q => q.ID == id);
+            Comment comment = _context.Comments.FirstOrDefault(q => q.ID == idb);
+
+
+            var id = comment.MovieID;
+            Movie movie = _context.Movies.Include(q => q.Comments.Where(q => q.IsDeleted == false)).FirstOrDefault(q => q.ID == id);
+            var name = movie.Name;
+
             comment.IsDeleted = true;
             _context.SaveChanges();
 
-            return Json("Silme işlemi başarılı!!");
+            string url = "/movie/" + id + "/" + UrlHelper.FriendlyUrl(name);
+            return Redirect(url);
+            //return Json("Silme işlemi başarılı!!");
         }
 
     }
